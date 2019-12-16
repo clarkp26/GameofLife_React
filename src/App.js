3f1503ea230a0736ba4fc84c784697f96e273e58
clarkp26 <35606607+clarkp26@users.noreply.github.com>
2019-12-16 12:53:57 +0000

    First React project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import Game from './Game.js'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    new Game()
+    // <div className="App">
+    //    <header className="App-header">
+      
+
+    //    </header>
+    //  </div>
+     
+  );
+}
+
+export default App;
